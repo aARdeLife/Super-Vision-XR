405803708cd6d0e21a715fc9df5f14f711758294
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-26 14:36:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const summaryBox = document.createElement('div');
+const switchCameraButton = document.getElementById('switch-camera');
 
 summaryBox.style.position = 'absolute';
 summaryBox.style.padding = '10px';
@@ -14,11 +15,26 @@ summaryBox.style.display = 'none';
 
 document.body.appendChild(summaryBox);
 
-async function setupCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video: { width: 640, height: 480 },
+let currentStream;
+
+async function setupCamera(deviceId = null) {
+    if (currentStream) {
+        currentStream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+
+    const constraints = {
+        video: {
+            width: 640,
+            height: 480,
+            deviceId: deviceId ? { exact: deviceId } : undefined
+        },
         audio: false
-    });
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    currentStream = stream;
     video.srcObject = stream;
     video.onloadedmetadata = () => {
         video.width = video.videoWidth;
@@ -33,6 +49,16 @@ async function setupCamera() {
         };
     });
 }
+
+switchCameraButton.addEventListener('click', async () => {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    const currentDevice = videoDevices.find(device => device.deviceId === currentStream.getVideoTracks()[0].getSettings().deviceId);
+    const nextDeviceIndex = (videoDevices.indexOf(currentDevice) + 1) % videoDevices.length;
+    const nextDevice = videoDevices[nextDeviceIndex];
+    await setupCamera(nextDevice.deviceId);
+    video.play();
+});
 
 function isPointInRect(x, y, rect) {
     return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
@@ -74,47 +100,48 @@ function getColorBySize(bbox) {
 
     const red = 255;
     const green = Math.floor(255 * ratio);
-    const blue = 0;
+const blue = 0;
 
-    return `rgb(${red}, ${green}, ${blue})`;
+return `rgb(${red}, ${green}, ${blue})`;
 }
 
 async function drawPredictions(predictions) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.font = '16px sans-serif';
-    ctx.textBaseline = 'top';
+ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+ctx.font = '16px sans-serif';
+ctx.textBaseline = 'top';
 
-    predictions.forEach(prediction => {
-        const x = prediction.bbox[0];
-        const y = prediction.bbox[1];
-        const width = prediction.bbox[2];
-        const height = prediction.bbox[3];
+predictions.forEach(prediction => {
+    const x = prediction.bbox[0];
+    const y = prediction.bbox[1];
+    const width = prediction.bbox[2];
+    const height = prediction.bbox[3];
 
-        ctx.strokeStyle = getColorBySize(prediction.bbox);
-        ctx.lineWidth = 2;
-        ctx.strokeRect(x, y, width, height);
+    ctx.strokeStyle = getColorBySize(prediction.bbox);
+    ctx.lineWidth = 2;
+    ctx.strokeRect(x, y, width, height);
 
-        ctx.fillStyle = getColorBySize(prediction.bbox);
-        ctx.fillText(prediction.class, x, y);
-    });
+    ctx.fillStyle = getColorBySize(prediction.bbox);
+    ctx.fillText(prediction.class, x, y);
+});
 }
 
 let currentPredictions = [];
 
 async function detectObjects() {
-    const model = await cocoSsd.load();
+const model = await cocoSsd.load();
 
-    async function detectFrame() {
-        currentPredictions = await model.detect(video);
-        drawPredictions(currentPredictions);
-        requestAnimationFrame(detectFrame);
-    }
+async function detectFrame() {
+    currentPredictions = await model.detect(video);
+    drawPredictions(currentPredictions);
+    requestAnimationFrame(detectFrame);
+}
 
-    detectFrame();
+detectFrame();
 }
 
 (async function() {
-    const videoElement = await setupCamera();
-    videoElement.play();
-    detectObjects();
+const videoElement = await setupCamera();
+videoElement.play();
+detectObjects();
 })();
+
